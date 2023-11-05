88429d9009df1386bf69466c193794dbf95d5985
Maksim <93036602+KREIK2017@users.noreply.github.com>
2023-11-05 18:22:11 +0200

    Add files via upload

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿using AspNetMVC.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetMVC.Controllers
 {
@@ -7,13 +6,7 @@ namespace AspNetMVC.Controllers
     {
         public IActionResult Index()
         {
-            var product = new List<Product>
-            {
-                new Product { ID = 1, Name = "Xiaomi", Price = 50.99, CreatedDate = DateTime.Now },
-                new Product { ID = 2, Name = "Apple", Price = 1500.80, CreatedDate = DateTime.Now.AddMinutes(-15) },
-                new Product { ID = 3, Name = "Asus", Price = 45.99, CreatedDate = DateTime.Now.AddDays(-2) }
-            };
-            return View(product);
+            return View("./Views/Product/Index.cshtml");
         }
     }
 }
